d2a3d9853e83e52f4a535722cd3c8558730a5076
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-02-02 13:32:35 -0500

    added validation and references to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   validates :name, format: { without: /\d/, message: "Can't contain numbers" }, length: { minimum: 2 }, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, length: { minimum: 6, maximum: 16}, presence: true
+  
+  has_many :posts
+  has_many :comments
 end
